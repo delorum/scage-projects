f912a380de20e975886073fb354123fa843b14b0
dunnololda <dunnolol.b@gmail.com>
2012-03-10 02:43:19 +0400

    stable version with health bars and flying damage numbers

--- jetflight/src/main/scala/net/scage/tutorial/netflight/NetFlight.scala
+++ jetflight/src/main/scala/net/scage/tutorial/netflight/NetFlight.scala
@@ -7,32 +7,78 @@ import net.scage.support.tracer3.{Trace, CoordTracer}
 import collection.mutable.{HashMap, ArrayBuffer}
 import net.scage.ScageApp
 
+class FlightTracer extends CoordTracer[Trace](solid_edges = false) {
+  private val _planes = HashMap[Int, ServerPlane]()
+  def plane(client_id:Int) = _planes(client_id)
+  def addPlane(coord:Vec, plane:ServerPlane) {
+    _planes += (plane.client_id -> plane)
+    addTrace(coord, plane)
+  }
+  def removePlane(client_id:Int) {
+    val plane = _planes(client_id)
+    plane.stop()
+    removeTraces(plane)
+    _planes -= client_id        
+  }
+}
+
 object NetFlight extends ScageApp(unit_name = "Net Flight") {
-  val tracer = CoordTracer(solid_edges = false)
-  private val planes = HashMap[Int, ServerPlane]()
+  val tracer = new FlightTracer  
+
+  private val count = HashMap[Int, (Int,  Int)]() // client -> (wins, deaths)
+  def countData:List[State] = {
+    (for {
+      (client_id, (wins, loses)) <- count
+    } yield State("client_id" -> client_id, 
+                  "wins" -> wins, 
+                  "loses" -> loses)).toList
+  }
+  private val count_updates = HashMap[Int, (Int,  Int)]() // client -> (wins, deaths)
+  def countUpdatesData:List[State] = {
+    (for {
+      (client_id, (wins, loses)) <- count_updates
+    } yield State("client_id" -> client_id, 
+                  "wins" -> wins, 
+                  "loses" -> loses)).toList
+  }
+  def fightResult(winer_id:Int, loser_id:Int) {
+    val (winer_wins, winer_loses) = count(winer_id)
+    count += (winer_id -> (winer_wins+1, winer_loses))
+    
+    val (loser_wins, loser_loses) = count(loser_id)
+    count += (loser_id -> (loser_wins, loser_loses+1))
+    
+    count_updates ++= Map(winer_id -> count(winer_id), loser_id -> count(loser_id))
+  }
   
-  val rockets = ArrayBuffer[ServerRocket]()
+  action(10000) {
+    new HealthBar
+  }
   
   val send_timeout = property("netflight.send_timeout", 50)
 
   NetServer.startServer(
     onClientAccepted = {
       client =>
-        planes += (client.id -> new ServerPlane(client.id))
-        client.send(State("your_plane_id" -> client.id))
+        new ServerPlane(client.id)
+        count += (client.id -> (0,0))
+        count_updates += (client.id -> (0,0))
+        client.send(State("your_plane_id" -> client.id, "count" -> countData))
     },
     onClientDataReceived = {
       (client, data) =>
-        val client_plane = planes(client.id)
         data.neededKeys {
           case ("keys", keys:List[Float]) =>
+            val client_plane = tracer.plane(client.id)
             keys.foreach(key => key match {
               case 0 => client_plane.rotateLeft()
               case 1 => client_plane.rotateRight()
               case 2 => client_plane.reheat()
-              case 3 => rockets += client_plane.fire
+              case 3 => client_plane.fire()
               case _ =>
             })
+          case ("goodbye", true) =>
+            NetServer.disconnectClient(client)
           /*case ("left", true) => client_plane.rotateLeft()
           case ("right", true) => client_plane.rotateRight()
           case ("up", true) => client_plane.reheat()*/
@@ -43,111 +89,172 @@ object NetFlight extends ScageApp(unit_name = "Net Flight") {
     },
     onClientDisconnected = {
       client =>
-        planes(client.id).stop()
-        planes -= client.id
+        tracer.removePlane(client.id)
+        count -= client.id
         NetServer.sendToAll(State("leaver" -> client.id))
     }
   )
   
   action(send_timeout) {
-    val data = State("planes" -> planes.values.map(_.toState).toList)
-    if(!rockets.isEmpty) data ++= State("rockets" -> rockets.map(_.toState).toList)
+    val data = State("planes_rockets" -> tracer.tracesList.map(_.state).toList)
+    if(!count_updates.isEmpty) {
+      data ++= State("count" -> countUpdatesData)
+      count_updates.clear()
+    }
     NetServer.sendToAll(data)
   }
 }
 
 import NetFlight._
 
-class ServerPlane(val client_id: Int) extends FlyingObject {
-  def toState = State("coord" -> trace.location,
-                      "health" -> _health,
-                      "id" -> client_id,
-                      "rotation" -> _rotation,
-                      "speed" -> _speed)
+class ServerPlane(val client_id: Int) extends FlyingObject with Trace {
+  tracer.addPlane(tracer.randomCoord(), this)
   
   protected var _health = 100
   def health = _health
 
   protected var plane_side = 1
 
-  private val trace = tracer.addTrace(tracer.randomCoord(), new Trace {
-    def state = State("type" -> "plane", "health" -> _health)
-
-    def changeState(changer: Trace, s: State) {
-      s.neededKeys {
-        case ("damage", damage: Int) => _health -= damage
-      }
+  def state = State("client_id" -> client_id,
+                    "coord" -> location,
+                    "health" -> _health,
+                    "rotation" -> _rotation,
+                    "speed" -> _speed,
+                    "type" -> "plane")
+  
+  def changeState(changer: Trace, s: State) {
+    s.neededKeys {
+      case ("damage", damage: Int) => 
+        _health -= damage
+        if(_health <= 0) changer.changeState(this, State("killer"))
+      case ("health", plus_health:Int) => _health += plus_health
     }
-  })
+  }
 
   private val action_id = action(10) {
     if (_health <= 0) {
       _health = 100
-      tracer.updateLocation(trace, tracer.randomCoord())
+      tracer.updateLocation(this, tracer.randomCoord())
       _rotation = (math.random * 20 - 10).toFloat
     }
 
-    tracer.updateLocation(trace, trace.location + step)
+    tracer.updateLocation(this, location + step)
     if (_speed > 5) _speed -= 0.1f
   }
   
-  def fire = {
-    try {new ServerRocket(trace.id,
-                          tracer.outsideCoord(trace.location + step.n.rotate(math.Pi/2 * plane_side)*10),
-                          speed, rotation)}
-    finally {plane_side *= -1}
+  def fire() {
+    new ServerRocket(this, tracer.outsideCoord(location + step.n.rotate(math.Pi/2 * plane_side)*10), speed, rotation)
+    plane_side *= -1
   }
 
   def stop() {
     delActions(action_id)
-    tracer.removeTraces(trace)
   }
 }
 
-class ServerRocket(shooter_id:Int, init_coord:Vec, init_speed:Float, init_rotation:Float) extends FlyingObject {
-  def toState = State("coord" -> trace.location, 
-                      "fuel" -> _fuel, 
-                      "id" -> trace.id, 
-                      "rotation" -> _rotation,
-                      "speed" -> _speed)
+class ServerRocket(shooter:ServerPlane, init_coord:Vec, init_speed:Float, init_rotation:Float) extends FlyingObject with Trace {
+  tracer.addTrace(init_coord, this)
   
   private var _fuel = 60
   _rotation = init_rotation
   _speed = init_speed + 10
 
-  val trace = tracer.addTrace(init_coord, new Trace {
-    def state = State("type" -> "rocket")
-    def changeState(changer:Trace, s:State) {}
-  })
+  def state = State("client_id" -> shooter.client_id,
+                    "coord" -> location,
+                    "fuel" -> _fuel, 
+                    "rocket_id" -> id,
+                    "rotation" -> _rotation,
+                    "speed" -> _speed,
+                    "type" -> "rocket")
+  def changeState(changer:Trace, s:State) {
+    s.neededKeys {
+      case ("killer", true) =>
+        fightResult(shooter.client_id, changer.state.value[Int]("client_id"))
+    }
+  }
 
   action(10) {
     if(_fuel > 0) {
-      tracer.updateLocation(trace, trace.location + step)
+      tracer.updateLocation(this, location + step)
 
-      val target_planes = tracer.tracesNearCoord(trace.location, -1 to 1,
+      val target_planes = tracer.tracesNearCoord(location, -1 to 1,
         condition = other_trace =>
-          other_trace.id != trace.id &&
-          other_trace.id != shooter_id &&
+          other_trace.id != id &&
+          other_trace.id != shooter.id &&
           other_trace.state.valueOrDefault("type", "unknown type") == "plane" &&
           other_trace.state.valueOrDefault("health", 0) > 0 &&
-          (other_trace.location dist trace.location) < (10+30))
+          (other_trace.location dist location) < (10+30))
       if(!target_planes.isEmpty) {
         val damage = (math.random*20).toInt
-        target_planes.foreach(_.changeState(trace, State("damage" -> damage)))
-        /*lazy val flying_word_color = shooter_id match {
-          case OurPlane.trace.id => RED
-          case EnemyPlane.trace.id => YELLOW
-          case _ => RED
-        }
-        new FlyingWord(damage, flying_word_color, trace.location, step)*/
+        target_planes.foreach(_.changeState(this, State("damage" -> damage)))
+        new ServerFlyingWord(shooter.client_id, damage, location, step)
         _fuel = 0
-      }
-
-      _fuel -= 1
+      } else _fuel -= 1
     } else {
-      rockets -= this
       deleteSelf()
-      tracer.removeTraces(trace)
+      tracer.removeTraces(this)
     }
   }
+}
+
+class ServerFlyingWord(client_id:Int, message:Any, init_coord:Vec, direction:Vec) extends FlyingObject with Trace {
+  tracer.addTrace(init_coord, this)
+
+  def state = State("client_id" -> client_id,
+                    "coord" -> location,
+                    "dir" -> _dir,
+                    "message" -> message.toString,
+                    "type" -> "word",
+                    "word_id" -> id)
+  def changeState(changer:Trace, s:State) {}
+
+  private var _lifetime = 10;
+
+  private val _dir = direction.n
+  private var _coord = init_coord
+
+  action(10) {
+    if(_lifetime > 0) {
+      _coord += _dir
+      _lifetime -= 1
+    } else {
+      deleteSelf()
+      tracer.removeTraces(this)
+    }
+  }
+}
+
+class HealthBar extends Trace {
+  tracer.addTrace(tracer.randomCoord(), this)
+  
+  def state = State("coord" -> location,
+                    "health_id" -> id,
+                    "type" -> "health")
+  def changeState(changer:Trace, s:State) {}
+
+  private var _lifetime = 3000; // 30 seconds
+  
+  action(10) {
+    if(_lifetime > 0) {
+      val planes_near = tracer.tracesInPoint(tracer.point(location), condition = {trace =>
+        trace.state.valueOrDefault("type", "unknown typ") == "plane" &&
+        (trace.location dist location) < (10+30)})
+      if(!planes_near.isEmpty) {
+        planes_near.head.changeState(this, State("health" -> 20))
+        _lifetime = 0
+      } else _lifetime -= 1
+    } else {
+      deleteSelf()
+      tracer.removeTraces(this)
+    }
+  }
+}
+
+class Ender(object_type:String,  object_id:Int) extends Trace {
+  tracer.addTrace(tracer.randomCoord(), this)
+
+  def state = State("object_id" -> object_id,
+                    "object_type" -> object_type,
+                    "type" -> "ender")
+  def changeState(changer:Trace, s:State) {}
 }
--- jetflight/src/main/scala/net/scage/tutorial/netflight/NetFlightClient.scala
+++ jetflight/src/main/scala/net/scage/tutorial/netflight/NetFlightClient.scala
@@ -3,68 +3,105 @@ package net.scage.tutorial.netflight
 import net.scage.ScageScreenApp
 import net.scage.ScageLib._
 import net.scage.support.net.NetClient
-import net.scage.support.{State, Vec}
 import collection.mutable.{ArrayBuffer, HashMap}
+import net.scage.support.{ScageColor, State, Vec}
 
 object NetFlightClient extends ScageScreenApp(unit_name = "Net Flight Client") {
-  val ENEMY_PLANE  = image("plane.png", 60, 60, 0, 0, 122, 121)
-  val OUR_PLANE    = image("plane2.png", 60, 60, 0, 0, 122, 121)
-  val ROCKET_ANIMATION = animation("rocket_animation.png", 10, 29, 14, 44, 3)
-  val EXPLOSION_ANIMATION = animation("explosion_animation.png", 36, 35, 72, 69, 3)
-  val LAND         = image("land.png", 800, 600, 0, 0, 800, 600)
+  val OUR_PLANE    = displayList {drawPolygon(Array(Vec(-15,-28), Vec(0,25), Vec(15,-28), Vec(0,-15)), RED)}
+  val ENEMY_PLANE  = displayList {drawPolygon(Array(Vec(-15,-28), Vec(0,25), Vec(15,-28), Vec(0,-15)), GREEN)}
+  val OUR_ROCKET   = displayList {drawRect(Vec(-2,4), 4, 8, RED)}
+  val ENEMY_ROCKET = displayList {drawRect(Vec(-2,4), 4, 8, GREEN)}
+
   val send_timeout = property("netflight.send_timeout", 50)
 
   private val planes = HashMap[Int, ClientPlane]()
-  
   val rockets = HashMap[Int, ClientRocket]()
-  
+  val words = HashMap[Int, ClientFlyingWord]()
+  val health_bars = HashMap[Int, ClientHealthBar]()
+
   private var our_plane_id = -1
   private var our_plane:Option[ClientPlane] = None
+  
+  private val count = HashMap[Int, (Int,  Int)]() // client -> (wins, deaths)
+  interface {
+    for {
+      ((client_id, (wins, loses)), index) <- count.zipWithIndex
+      is_our_count = client_id == our_plane_id
+    } print(client_id+": "+wins+" / "+loses, 20, window_height-20-20*index, if(is_our_count) RED else GREEN)
+
+    print("Health: "+(if(our_plane.isDefined) our_plane.get.health else 0), 20, 20, RED)
+  }
 
   NetClient.startClient(
     onServerDataReceived = {
-      data => data.neededKeys {
-        case ("planes", planes_data:List[State]) =>
+      data => synchronized {data.neededKeys {
+        case ("planes_rockets", planes_data:List[State]) =>
           planes_data.foreach(plane_data => plane_data match {
-            case State(("coord", coord:Vec),
+            case State(("client_id", client_id:Float),
+                       ("coord", coord:Vec),
                        ("health", health:Float),
-                       ("id", id:Float),
                        ("rotation", rotation:Float),
-                       ("speed", speed:Float)) =>
-              planes.get(id.toInt) match {
+                       ("speed", speed:Float),
+                       ("type", "plane")) =>
+              planes.get(client_id.toInt) match {
                 case Some(plane) => plane.update(coord, rotation, speed, health.toInt)
                 case None =>
-                  val new_plane = if(id.toInt == our_plane_id) {
-                    val plane = new ClientPlane(id.toInt, coord, rotation, speed, health.toInt, OUR_PLANE)
+                  val new_plane = if(client_id.toInt == our_plane_id) {
+                    val plane = new ClientPlane(client_id.toInt, coord, rotation, speed, health.toInt, OUR_PLANE, RED)
                     our_plane = Some(plane)
                     plane
-                  } else new ClientPlane(id.toInt, coord, rotation, speed, health.toInt)
-                  planes += (id.toInt -> new_plane)
+                  } else new ClientPlane(client_id.toInt, coord, rotation, speed, health.toInt)
+                  planes += (client_id.toInt -> new_plane)
+              }
+            case State(("client_id", client_id:Float),
+                       ("coord", coord:Vec),
+                       ("fuel", fuel:Float),
+                       ("rocket_id", rocket_id:Float),
+                       ("rotation", rotation:Float),
+                       ("speed", speed:Float),
+                       ("type", "rocket")) =>
+              rockets.get(rocket_id.toInt) match {
+                case Some(rocket) => rocket.update(coord, rotation, speed, fuel.toInt)
+                case None =>
+                  val new_rocket = new ClientRocket(rocket_id.toInt, coord, rotation, speed, fuel.toInt, if(client_id.toInt == our_plane_id) OUR_ROCKET else ENEMY_ROCKET)
+                  rockets += (rocket_id.toInt -> new_rocket)
+              }
+            case State(("client_id", client_id:Float),
+                       ("coord", coord:Vec),
+                       ("dir", dir:Vec),
+                       ("message", message:String),
+                       ("type", "word"),
+                       ("word_id", word_id:Float)) =>
+              if(!words.contains(word_id.toInt)) {
+                val new_word = new ClientFlyingWord(word_id.toInt, message, coord, dir, if(client_id.toInt == our_plane_id) RED else GREEN)
+                words += (word_id.toInt -> new_word)
+              }
+            case State(("coord", coord:Vec),
+                       ("health_id", health_id:Float),
+                       ("type", "health")) =>
+              health_bars.get(health_id.toInt) match {
+                case Some(health_bar) => health_bar.addLifeTime()
+                case None =>
+                  val new_health_bar = new ClientHealthBar(health_id.toInt, coord)
+                  health_bars += (health_id.toInt -> new_health_bar)
               }
             //case _ =>
         })
-        case ("rockets", rockets_data:List[State]) =>
-          rockets_data.foreach(rocket_data => rocket_data match {
-            case State(("coord", coord:Vec),
-                       ("fuel", fuel:Float),
-                       ("id", id:Float),
-                       ("rotation", rotation:Float),
-                       ("speed", speed:Float)) =>
-              rockets.get(id.toInt) match {
-                case Some(rocket) => rocket.update(coord, rotation, speed, fuel.toInt)
-                case None =>
-                  val new_rocket = new ClientRocket(id.toInt, coord, rotation, speed, fuel.toInt)
-                  rockets += (id.toInt -> new_rocket)
-              }
-            //case _ =>
-          })
+        case ("count", count_data:List[State]) =>
+          count_data.foreach(count_for_client => count_for_client match {
+            case State(("client_id", client_id:Float), 
+                       ("loses", loses:Float), 
+                       ("wins", wins:Float)) =>
+              count += (client_id.toInt -> (wins.toInt, loses.toInt))
+          })          
         case ("your_plane_id", id:Float) =>
           our_plane_id = id.toInt
         case ("leaver", id:Float) =>
           planes.get(id.toInt).map(_.stop())
           planes -= id.toInt
+          count -= id.toInt
       }        
-    }
+    }}
   )
 
   val LEFT = 0
@@ -85,7 +122,7 @@ object NetFlightClient extends ScageScreenApp(unit_name = "Net Flight Client") {
     our_plane.map(_.reheat())
   })
   
-  key(KEY_LCONTROL, onKeyDown = {
+  key(KEY_LCONTROL, 200, onKeyDown = {
     keyboard_buffer += LCTRL
     //our_plane.map(_.fire())
   })
@@ -95,13 +132,16 @@ object NetFlightClient extends ScageScreenApp(unit_name = "Net Flight Client") {
     keyboard_buffer.clear()
   }
   
-  backgroundColor = WHITE
-  render(-10) {
+  backgroundColor = BLACK
+  /*render(-10) {
     drawDisplayList(LAND, windowCenter)
-  }
+  }*/
 
   dispose {
+    NetClient.sendSync(State("goodbye"))
     NetClient.stopClient()
+
+    /*Thread.sleep(5000)*/
   }
 }
 
@@ -112,7 +152,7 @@ class ClientPlane(val client_id: Int,
                   init_rotation:Float, 
                   init_speed:Float, 
                   init_health:Int,
-                  image_code:Int = ENEMY_PLANE) extends FlyingObject {
+                  image_code:Int = ENEMY_PLANE, health_color:ScageColor = GREEN) extends FlyingObject {
   _rotation = init_rotation
   _speed = init_speed
   
@@ -158,7 +198,7 @@ class ClientPlane(val client_id: Int,
       openglRotate(_rotation)
       drawDisplayList(image_code, Vec.zero)
     }
-    print(_health, Vec.zero, YELLOW)
+    //drawFilledRect(Vec(-30,-30), 60f*(_health/100), 5, health_color)
   }
   
   def stop() {
@@ -166,11 +206,12 @@ class ClientPlane(val client_id: Int,
   }
 }
 
-class ClientRocket(val rocket_id: Int, 
+class ClientRocket(val rocket_id: Int,
                    init_coord:Vec, 
                    init_rotation:Float, 
                    init_speed:Float, 
-                   init_fuel:Int) extends FlyingObject {
+                   init_fuel:Int,
+                   rocket_image_code:Int) extends FlyingObject {
   _rotation = init_rotation
   _speed = init_speed
   
@@ -220,11 +261,58 @@ class ClientRocket(val rocket_id: Int,
     if(fuel > 0) {
       openglMove(_coord)
       openglRotate(rotation)
-      drawDisplayList(ROCKET_ANIMATION(0), Vec.zero)
+      drawDisplayList(rocket_image_code, Vec.zero)
     } else {
-      openglMove(_coord)
-      drawDisplayList(EXPLOSION_ANIMATION(0), Vec.zero)
+      /*openglMove(_coord)
+      drawDisplayList(EXPLOSION_ANIMATION(0), Vec.zero)*/
       deleteSelf()
     }
+  }
+}
+
+class ClientFlyingWord(rocket_id:Int, message:Any, init_coord:Vec, direction:Vec, color:ScageColor) extends FlyingObject {
+  private var _lifetime = 60;
+  private var _coord = init_coord
+  private val _dir = direction.n
+
+  action(10) {
+    if(_lifetime > 0) {
+      _coord += _dir
+      _lifetime -= 1
+    } else {
+      deleteSelf()
+      words -= rocket_id
+    }
+  }
+
+  render {
+    if(_lifetime > 0) {
+      print(message, _coord, color)
+    } else deleteSelf()
+  }
+}
+
+class ClientHealthBar(health_id:Int, coord:Vec) {
+  private var _lifetime = 30;
+
+  def addLifeTime() {
+    _lifetime = 30
+  }
+
+  action(10) {
+    _lifetime -= 1
+    if(_lifetime <= 0) {
+      deleteSelf()
+      health_bars -= health_id
+    }
+  }
+
+  render {
+    if(_lifetime > 0) {
+      openglMove(coord)
+      drawRectCentered(Vec.zero, 30, 30, RED)
+      drawLines(Vec(0, 13), Vec(0, -13),
+                Vec(-13, 0), Vec(13, 0))
+    } else deleteSelf()
   }
 }
