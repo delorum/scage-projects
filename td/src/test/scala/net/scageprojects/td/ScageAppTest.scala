4c99ffb856859f1c60ea5b7902d31a08ae34a248
dunnololda <dunnolol.b@gmail.com>
2012-05-05 09:37:11 +0400

    td started

---
+++ td/src/test/scala/net/scageprojects/td/ScageAppTest.scala
@@ -0,0 +1,32 @@
+package net.scageprojects.td;
+
+import junit.framework._;
+import Assert._;
+
+object ScageAppTest {
+    def suite: Test = {
+        val suite = new TestSuite(classOf[ScageAppTest]);
+        suite
+    }
+
+    def main(args : Array[String]) {
+        junit.textui.TestRunner.run(suite);
+    }
+}
+
+/**
+ * Unit test for simple App.
+ */
+class ScageAppTest extends TestCase("app") {
+
+    /**
+     * Rigourous Tests :-)
+     */
+    def testOK() = {
+      ScageExampleApp.main(Array[String]())
+      assertTrue(true)
+    };
+    //def testKO() = assertTrue(false);
+    
+
+}
