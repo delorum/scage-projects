bec1c67edd32c50c40e1ada03db6c65900a1dca6
dunnololda <dunnolol.b@gmail.com>
2013-03-12 09:40:09 +0400

    added drivecontrol

---
+++ drivecontrol/run-scripts/windows/run.bat
@@ -0,0 +1,1 @@
+java -Dscage.properties=${scageproperties} -Djava.library.path=native -Dfile.encoding=UTF-8 -jar lib/${artifactId}-${version}.jar
---
+++ drivecontrol/src/main/scala/net/scageprojects/drivecontrol/DriveControl.scala
@@ -0,0 +1,12 @@
+package net.scageprojects.drivecontrol
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dunno
+ * Date: 12.03.13
+ * Time: 9:28
+ * To change this template use File | Settings | File Templates.
+ */
+object DriveControl {
+
+}
