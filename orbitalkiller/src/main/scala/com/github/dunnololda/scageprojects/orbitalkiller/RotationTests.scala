b898acf6c49c0d617c0ee8371ce00f0a265e11aa
dunnololda <dunnolol.b@gmail.com>
2013-07-11 09:58:13 +0400

    rotation tests - found solution

--- orbitalkiller/src/main/scala/com/github/dunnololda/scageprojects/orbitalkiller/RotationTests.scala
+++ orbitalkiller/src/main/scala/com/github/dunnololda/scageprojects/orbitalkiller/RotationTests.scala
@@ -1,21 +1,60 @@
 package com.github.dunnololda.scageprojects.orbitalkiller
 
 import com.github.dunnololda.scage.ScageLib._
+import org.lwjgl.opengl.GL11
 
 object RotationTests extends ScageScreenApp("Rotation Test", 640 ,480) {
+  var rotation = 0f
+  action(50) {
+    rotation = (rotation+0.05f) % (2*math.Pi.toFloat)
+  }
   val c = Vec(320, 240)
   render {
     openglLocalTransform {
-      openglMove(c)
-      openglRotateDeg(45)
+      //openglMove(Vec(40,40))
+      //openglMove(c-Vec(40,40))
+      //openglRotateRad(-rotation)
+      /*openglMove((c-Vec(40,40))*(-1))*/
+      openglMove(c-Vec(40,40))
+
+      GL11.glPushMatrix()
       openglMove(Vec(40, 40))
+      openglRotateRad(-rotation)
+      openglMove(Vec(-40, -40))
+
+      openglMove(Vec(40, 40))
+      openglRotateRad(rotation)
       drawRectCentered(Vec.zero, 20, 20)
-      openglMove(Vec(0, -80))
+      GL11.glPopMatrix()
+
+      //openglRotateRad(-rotation)
+
+      GL11.glPushMatrix()
+      openglMove(Vec(40, 40))
+      openglRotateRad(-rotation)
+      openglMove(Vec(-40, -40))
+
+      openglMove(Vec(40, -40))
       drawRectCentered(Vec.zero, 20, 20)
-      openglMove(Vec(-80, 0))
+      GL11.glPopMatrix()
+
+      GL11.glPushMatrix()
+      openglMove(Vec(40, 40))
+      openglRotateRad(-rotation)
+      openglMove(Vec(-40, -40))
+
+      openglMove(Vec(-40, -40))
       drawRectCentered(Vec.zero, 20, 20)
-      openglMove(Vec(0, 80))
+      GL11.glPopMatrix()
+
+      GL11.glPushMatrix()
+      openglMove(Vec(40, 40))
+      openglRotateRad(-rotation)
+      openglMove(Vec(-40, -40))
+
+      openglMove(Vec(-40, 40))
       drawRectCentered(Vec.zero, 20, 20)
+      GL11.glPopMatrix()
     }
   }
 }
