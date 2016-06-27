58a896799a7e6ff8cf483982f5c4db43dc5b5f3a
Andrey Borunov <a.borunov@netris.ru>
2016-06-27 15:17:38 +0300

    orbitalkiller: updated time-step choosing algorithm, the new one seems more accurate

--- orbitalkiller/src/main/scala/com/github/dunnololda/scageprojects/orbitalkiller/RealTrajectory.scala
+++ orbitalkiller/src/main/scala/com/github/dunnololda/scageprojects/orbitalkiller/RealTrajectory.scala
@@ -34,45 +34,36 @@ class RealTrajectoryC(max_multiplier:Option[Double]) {
   private var min_m:Double = Double.MaxValue
   private var max_m:Double = 0
 
+  private val calc_multiplier:() => Double = {
+    def m:Double = (for {
+      ps <- system_evolution_copy.bodyState(player_ship.index)
+    } yield {
+      // http://arxiv.org/pdf/1105.1082.pdf
+      // N-body simulations of gravitational dynamics, Walter Dehnen and Justin I. Read,
+      // p. 7, 2.2.3 The choise of time-step, formula (21)
+      // Квадратный корень от softening length, деленного на ускорение в точке, умноженный на коэффициент.
+      // Softening length посчитал как радиус системы (380к км) поделить на квадратный корень от количества взаимодействующих тел (то есть,
+      // корень из трех - Земля, Луна, корабль). Коэффициент подобран так, чтобы минимальный шаг был base_dt.
+      val m = 1.0/543200*math.sqrt(2.2E8 / ps.acc.norma)/base_dt
+      if (m < min_m) min_m = m
+      if (m > max_m) max_m = m
+      m
+    }).getOrElse(1.0)
+    max_multiplier.map(x => {
+      if(x == 1) () => 1.0
+      else {
+        () => math.min(math.max(1, m), x)
+      }
+    }).getOrElse({
+      () => math.max(1, m)
+    })
+  }
+
   protected def chooseDt:Double = {
     if(player_ship.engines.exists(_.stopMomentTacts >= system_evolution_copy.tacts)) {
       base_dt // пока работают двигатели, dt должен быть равен base_dt, иначе неверно работают формулы.
     } else {
-      if(max_multiplier.exists(_ == 1)) base_dt
-      else {
-        (for {
-          ss <- system_evolution_copy.bodyState(sun.index)
-          es <- system_evolution_copy.bodyState(earth.index)
-          ms <- system_evolution_copy.bodyState(moon.index)
-          ps <- system_evolution_copy.bodyState(player_ship.index)
-        } yield {
-          // http://arxiv.org/pdf/1105.1082.pdf
-          // N-body simulations of gravitational dynamics, Walter Dehnen and Justin I. Read,
-          // p. 7, 2.2.3 The choise of time-step, formula (20)
-          // Считаем гравитационный потенциал в точке, от модуля извлекаем квадртаный корень и делим на модуль ускорения корабля.
-          // И умножаем на коэффициент. Эмпирически подобрал коэффициент 1/25565. При этом минимальный dt будет base_dt (1/63 секунды),
-          // максимальный в 6000 больше.
-          val x = math.sqrt(G * (/*sun.mass/ss.coord.dist(ps.coord) +
-                             */ earth.mass / es.coord.dist(ps.coord) +
-            moon.mass / ms.coord.dist(ps.coord))) / ps.acc.norma
-          //val m = x/25565/base_dt
-          val m = x / 5600 / base_dt
-          if (m < min_m) min_m = m
-          if (m > max_m) max_m = m
-          println(f"x = $m*base_dt, min_m = $min_m, max_m = $max_m")
-          if(max_multiplier.isEmpty) m * base_dt
-          else math.min(m, max_multiplier.get)*base_dt
-        }).getOrElse(base_dt)
-      }
-      /*val multiplier = system_evolution_copy.bodyState(player_ship.thisOrActualProxyShipIndex).map(bs => {
-        val (min_dist_to_any_surface, planet_mass) = celestials.map(x => (bs.coord.dist(x._2.coord) - x._1.radius, x._2.mass)).minBy(_._1)
-        if(min_dist_to_any_surface >= 10000000) {
-          math.max(1.0, math.min(30000.0/100000 * min_dist_to_any_surface / bs.vel.norma, max_multiplier.getOrElse(Double.MaxValue)))
-        } else {
-          math.max(1.0, math.min(30000.0/100000 * min_dist_to_any_surface / bs.vel.norma * moon.mass / planet_mass, max_multiplier.getOrElse(Double.MaxValue)))
-        }
-      }).getOrElse(1.0)
-      multiplier*base_dt*/
+      calc_multiplier()*base_dt
     }
   }
 
@@ -135,7 +126,7 @@ class RealTrajectoryC(max_multiplier:Option[Double]) {
       if(curPoints > InterfaceHolder.realTrajectorySwitcher.numPoints) {
         InterfaceHolder.realTrajectorySwitcher.numPoints = curPoints
       }
-      println(f"real trajectory dt ${system_evolution_copy.base_dt/base_dt}%.2f*base_dt curPoints/numPoints $curPoints/${InterfaceHolder.realTrajectorySwitcher.numPoints} dropped/length $dropped/${real_trajectory.length}")
+      println(f"real trajectory dt ${system_evolution_copy.base_dt/base_dt}%.2f*base_dt, min_m = $min_m, max_m = $max_m, curPoints/numPoints $curPoints/${InterfaceHolder.realTrajectorySwitcher.numPoints} dropped/length $dropped/${real_trajectory.length}")
     }
   }
 
